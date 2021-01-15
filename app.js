60b3d297d23f550e7a3c4e6d85515b4bcdd5faad
PHANTOM-14 <73062410+PHANTOM-14@users.noreply.github.com>
2021-01-15 14:51:17 +0530

    Updated Giveaway
    Now GiveAways are working perfectly

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
 bot.commands = new Discord.Collection();
-const db = require ("quick.db");
+const db = require('quick.db')
 
 fs.readdir("./commands/", (err, files) => {
 
@@ -28,94 +28,26 @@ const { GiveawaysManager } = require('discord-giveaways');
 const manager = new GiveawaysManager(bot, {
     storage: './giveaways.json',
     updateCountdownEvery: 10000,
-    hasGuildMembersIntent: true,
+    hasGuildMembersIntent: false,
     default: {
         botsCanWin: false,
         exemptPermissions: ['MANAGE_MESSAGES', 'ADMINISTRATOR'],
         embedColor: '#FF0000',
-        reaction: '<:Giveaway:795287777943486464>'
+        reaction: '🎉'
     }
 });
 // We now have a giveawaysManager property to access the manager everywhere!
 bot.giveawaysManager = manager;
 
 bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
+  console.log(`${bot.user.username} is online on ${bot.guilds.cache.size} servers!`);
 
   bot.user.setActivity("Prefix is s! use s!invite", {type: "PLAYING"});
 });
 
-bot.on("message", async (message) => {
-if(message.author.bot) return;
-let substringArray = get_substrings_between(message.content, ":", ":");
-let msg = message.content;
-if(!substringArray.length) return;
-
-substringArray.forEach(m => {
-    let emoji = bot.emojis.cache.find(x => x.name === m);
-    var replace = `:${m}:`;
-    var rexreplace = new RegExp(replace, 'g');
-
-    if(emoji && !msg.split(" ").find(x => x === emoji.toString()) && !msg.includes(`<a${replace}${emoji.id}>`)) msg = msg.replace(rexreplace, emoji.toString());
-})
 
 
-if(msg === message.content) return;
-
-let webhook = await message.channel.fetchWebhooks();
-webhook = webhook.find(x => x.name === "NQN2");
-
-if(!webhook) {
-    webhook = await message.channel.createWebhook(`NQN2`, {
-        avatar: bot.user.displayAvatarURL({dynamic: true})
-    });
-}
-
-await webhook.edit({
-    name: message.member.nickname ? message.member.nickname : message.author.username,
-    avatar: message.author.displayAvatarURL({dynamic: true})
-})
-
-message.delete().catch(m => {})
-
-webhook.send(msg).catch( m => {});
-
-await webhook.edit({
-    name: `NQN2`,
-    avatar: bot.user.displayAvatarURL({dynamic:true})
-})
-
-
-})
-//--------------------------------------------------- F U N C T I O N S --------------------------------------
-
-function get_substrings_between(str, startDelimiter, endDelimiter) {
-var contents = [];
-var startDelimiterLength = startDelimiter.length;
-var endDelimiterLength = endDelimiter.length;
-var startFrom = contentStart = contentEnd = 0;
-
-while (false !== (contentStart = strpos(str, startDelimiter, startFrom))) {
-  contentStart += startDelimiterLength;
-  contentEnd = strpos(str, endDelimiter, contentStart);
-  if (false === contentEnd) {
-    break;
-  }
-  contents.push(str.substr(contentStart, contentEnd - contentStart));
-  startFrom = contentEnd + endDelimiterLength;
-}
-
-return contents;
-}
-
-
-function strpos(haystack, needle, offset) {
-var i = (haystack + '').indexOf(needle, (offset || 0));
-return i === -1 ? false : i;
-}
-
-bot.on("message", async message => {
-
+bot.on("message", async message => { 
   if(db.has(`afk-${message.author.id}+${message.guild.id}`)) {
     const info = db.get(`afk-${message.author.id}+${message.guild.id}`)
     await db.delete(`afk-${message.author.id}+${message.guild.id}`)
@@ -127,6 +59,7 @@ bot.on("message", async message => {
         message.channel.send('`'+message.mentions.members.first().user.tag + "` is currently afk \n **REASON-> **" + db.get(`afk-${message.mentions.members.first().id}+${message.guild.id}`))
     }else return;
   }else;
+
 
   if(message.author.bot) return;
   if(message.channel.type === "dm") return;
@@ -140,7 +73,6 @@ bot.on("message", async message => {
   
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
   if(commandfile) commandfile.run(bot,message,args);
-
 });
 
 bot.login(token.token);
--- commands/gend.js
+++ commands/gend.js
@@ -4,12 +4,12 @@ exports.run = async (bot, message, args) => {
 
     // If the member doesn't have enough permissions
     if(!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")){
-        return message.channel.send('<a:RedTick:792354166663282728> You need to have the manage messages permissions or Role `Giveaways` to reroll giveaways.');
+        return message.channel.send(':x: You need to have the manage messages permissions to reroll giveaways.');
     }
 
     // If no message ID or giveaway name is specified
     if(!args[0]){
-        return message.channel.send('<a:RedTick:792354166663282728>  You have to specify a valid message ID!');
+        return message.channel.send(':x: You have to specify a valid message ID!');
     }
 
     // try to found the giveaway with prize then with ID
