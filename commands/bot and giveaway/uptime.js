2bac91b102b2aaec584cff7e23b83df39fd65114
PHANTOM-14 <73062410+PHANTOM-14@users.noreply.github.com>
2021-04-01 14:18:05 +0530

    Updates Uptime

--- commands/bot and giveaway/uptime.js
+++ commands/bot and giveaway/uptime.js
@@ -1,22 +1,23 @@
 const { MessageEmbed } = require('discord.js');
 const moment = require("moment");
+const Discord = require("discord.js")
+
 module.exports = {
     name: 'uptime',
     aliases: ['online'],
     description: 'Shows the uptime of the bot.',
     cooldown: 5,
     async execute(message) {
-        require("moment-duration-format");
-        const duration = moment.duration(bot.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
+        const bot = message.client;
+       require("moment-duration-format");
+       const duration = moment.duration(bot.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
 
-        console.log(duration);
+        console.log(bot.uptime);
 
         let serverembed = new MessageEmbed()
             .setColor("#00ff62")
             .addField('I am online from', duration);
 
         message.channel.send(serverembed);
-
-
     }
 }
