c0f207e127e9887d75b15f67652b5701dd9cf271
PHANTOM-14 <73062410+PHANTOM-14@users.noreply.github.com>
2021-04-09 12:11:50 +0530

    Update guildmemberadd.js

--- events/guildmemberadd.js
+++ events/guildmemberadd.js
@@ -2,13 +2,12 @@ const db = require('quick.db')
 module.exports = {
     name: 'guildMemberAdd',
     once: true,
-    execute(client) {
+    execute(member ,client) {
 
-        client.on("guildMemberAdd", (member) => {
-            guild = member.guild;
-            const wmsg = db.get(`wmsg-${guild.id}`);
-            const wch = db.get(`wch-${guild.id}`);
-            guild.channels.cache.get(wch).send(`<@${member.user.id}> ${wmsg}`);
-        });
+        guild = member.guild;
+        const wmsg = db.get(`wmsg-${guild.id}`);
+        const wch = db.get(`wch-${guild.id}`);
+        guild.channels.cache.get(wch).send(`<@${member.user.id}> ${wmsg}`);
     }
+
 }
