094ff8727bfec39efc3b6676e26d14a24a626ecc
Phantom <73062410+PHANTOM-14@users.noreply.github.com>
2021-05-01 19:45:39 +0530

    Added Setup Commands ✅

---
+++ commands/setup commands/logs-channel.js
@@ -0,0 +1,29 @@
+const { Discord } = require('discord.js');
+const db = require('quick.db')
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: 'logschannel',
+    aliases: ['logs', 'setlogs','setlog'],
+    description: 'Setup the channel where welcome message will be posted',
+
+    async execute(message, args, client) {
+        let userinfoget =
+            message.mentions.members.first() ||
+            message.guild.members.cache.get(args[0]) ||
+            message.guild.member(message.author);
+
+        guild = message.guild;
+        if (!args[0]) return message.reply(`mention a valid channel!!`)
+        let content = message.mentions.channels.first().id;
+
+        if (!content) return message.reply(`mention a valid channel!!`)
+
+        await db.set(`logch-${message.guild.id}`, content)
+        const embed = new MessageEmbed()
+            .setDescription(`You have been setted logs channel to <#${content}>`)
+            .setColor(userinfoget.displayHexColor)
+            .setAuthor(message.author.tag, message.author.displayAvatarURL({ dynamic: true }))
+        message.channel.send(embed)
+    }
+}
