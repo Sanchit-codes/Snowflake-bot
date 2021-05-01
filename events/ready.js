389aebe6fa6e6b0d41b8d07b10c946dec9fb794a
Phantom <73062410+PHANTOM-14@users.noreply.github.com>
2021-05-01 19:41:24 +0530

    Updating Events

--- events/ready.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-        console.log(`Guilds: ${client.guilds.cache.size}\nUsers: ${client.users.cache.size}\n`);
-        client.user.setActivity(`${client.users.cache.size} Users | Snowflake prefix`, { type: "WATCHING" });
-	},
-};
